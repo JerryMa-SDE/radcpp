bc63eac18fc24164d84a5071215488daa83bdd13
Jerry Ma <majunlichn@outlook.com>
2023-08-22 00:38:03 +0800

    rad/Core/Span
    Span refers to a contiguous sequence of objects, inspired by:
    https://llvm.org/doxygen/ArrayRef_8h_source.html.
    The design is more flexible with less restrictions compared to std::span/gsl::span.

--- rad/Core/Global.h
+++ rad/Core/Global.h
@@ -2,3 +2,16 @@
 
 #define RAD_BEGIN_NAMESPACE namespace rad {
 #define RAD_END_NAMESPACE }
+
+#ifndef RAD_GNUC_PREREQ
+#if defined(__GNUC__) && defined(__GNUC_MINOR__) && defined(__GNUC_PATCHLEVEL__)
+#define RAD_GNUC_PREREQ(major, minor, patch) \
+            ((__GNUC__ << 20) + (__GNUC_MINOR__ << 10) + __GNUC_PATCHLEVEL__ >= \
+                ((major) << 20) + ((minor) << 10) + (patch))
+#elif defined(__GNUC__) && defined(__GNUC_MINOR__)
+#define RAD_GNUC_PREREQ(major, minor, patch) \
+            ((__GNUC__ << 20) + (__GNUC_MINOR__ << 10) >= ((major) << 20) + ((minor) << 10))
+#else
+#define RAD_GNUC_PREREQ(major, minor, patch) 0
+#endif
+#endif // RAD_GNUC_PREREQ
