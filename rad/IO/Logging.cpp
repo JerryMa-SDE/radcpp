5e4f1226490a2c7489195432dcdcac6b3a610136
Jerry Ma <majunlichn@outlook.com>
2023-08-25 04:06:02 +0800

    rad/IO/Logging

---
+++ rad/IO/Logging.cpp
@@ -0,0 +1,93 @@
+#include "rad/IO/Logging.h"
+#include "rad/IO/File.h"
+
+#include <cstdarg>
+
+RAD_BEGIN_NAMESPACE
+
+#ifndef RAD_NO_LOGGING
+
+const char* g_logLevelStrings[int(LogLevel::Count)] =
+{
+    "Undefined",
+    "Verbose",
+    "Debug",
+    "Info",
+    "Warn",
+    "Error",
+    "Critical",
+};
+
+const char* GetLogLevelString(LogLevel level)
+{
+    return g_logLevelStrings[int(level)];
+}
+
+void LogCategory::Print(LogLevel level, const char* format, ...)
+{
+    std::chrono::system_clock::time_point timestamp = std::chrono::system_clock::now();
+
+    thread_local std::string message;
+    va_list args;
+    va_start(args, format);
+    StrPrintInPlaceArgList(message, format, args);
+    va_end(args);
+
+    long long milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
+        timestamp.time_since_epoch()).count() % 1000;
+    std::time_t timepoint = std::chrono::system_clock::to_time_t(timestamp);
+    std::tm datetime = {};
+#if defined(_WIN32) || defined(_WIN64)
+    localtime_s(&datetime, &timepoint);
+#else
+    localtime_r(&timepoint, &datetime);
+#endif
+
+    thread_local std::string buffer;
+    int charsPrinted = StrPrintInPlace(buffer, "[%02d:%02d:%02d.%03lld] %s: %s: %s\n",
+        datetime.tm_hour, datetime.tm_min, datetime.tm_sec, milliseconds,
+        m_name.data(), g_logLevelStrings[int(level)], message.data());
+
+    if ((charsPrinted > 0) && (level >= m_outputLevel))
+    {
+        Write(buffer.data(), charsPrinted);
+    }
+
+    if (level >= m_flushLevel)
+    {
+        Flush();
+    }
+}
+
+void LogCategory::Write(const char* data, size_t sizeInBytes)
+{
+    std::lock_guard lockGuard(s_outputMutex);
+
+    if (!s_logFile.IsOpen())
+    {
+        std::string processName = (const char*)GetCurrentProcessPath()
+            .filename().u8string().c_str();
+        s_logFile.Open(processName + ".log", FileAccessWrite);
+    }
+
+    if (s_logFile.IsOpen())
+    {
+        s_logFile.Write(data, sizeInBytes);
+    }
+
+    if (m_enableConsoleOutput)
+    {
+        fprintf(stderr, "%s", data);
+    }
+}
+
+void LogCategory::Flush()
+{
+    s_logFile.Flush();
+}
+
+#endif // #ifndef RAD_NO_LOGGING
+
+RAD_END_NAMESPACE
+
+RAD_LOG_CATEGORY_DEFINE(Global);
---
+++ rad/IO/Logging.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "rad/Core/Global.h"
+#include "rad/IO/File.h"
+#include <chrono>
+#include <mutex>
+
+RAD_BEGIN_NAMESPACE
+
+#ifndef RAD_NO_LOGGING
+
+enum class LogLevel
+{
+    Undefined,
+    Verbose,
+    Debug,
+    Info,
+    Warn,
+    Error,
+    Critical,
+    Count
+};
+
+const char* GetLogLevelString(LogLevel level);
+
+class LogCategory
+{
+public:
+    LogCategory() = default;
+    LogCategory(std::string_view name, LogLevel outputLevel) :
+        m_name(name),
+        m_outputLevel(outputLevel)
+    {
+    }
+    ~LogCategory()
+    {
+    }
+
+    void SetOutputLevel(LogLevel outputLevel)
+    {
+        m_outputLevel = outputLevel;
+    }
+
+    void SetFlushLevel(LogLevel flushLevel)
+    {
+        m_flushLevel = flushLevel;
+    }
+
+    void Print(LogLevel level, const char* format, ...);
+    void Write(const char* data, size_t sizeInBytes);
+    void Flush();
+
+protected:
+    std::string m_name;
+    LogLevel m_outputLevel;
+    LogLevel m_flushLevel = LogLevel::Warn;
+
+    static std::mutex s_outputMutex;
+    static rad::File s_logFile;
+    bool m_enableConsoleOutput = true;
+
+}; // class LogCategory
+
+#define RAD_LOG_CATEGORY(Name) g_logCategory##Name
+#define RAD_LOG_CATEGORY_DECLARE(Name, OutputLevel) \
+class LogCategory##Name : public rad::LogCategory \
+{ \
+public: \
+    LogCategory##Name() : LogCategory(#Name, rad::LogLevel::OutputLevel) {} \
+}; \
+extern LogCategory##Name RAD_LOG_CATEGORY(Name);
+#define RAD_LOG_CATEGORY_DEFINE(Name) LogCategory##Name RAD_LOG_CATEGORY(Name);
+#define RAD_LOG(Name, Level, Format, ...) RAD_LOG_CATEGORY(Name).Print(rad::LogLevel::Level, Format, ##__VA_ARGS__)
+#define RAD_LOG_SET_OUTPUT_LEVEL(Name, Level) RAD_LOG_CATEGORY(Name).SetOutputLevel(LogLevel::Level)
+#define RAD_LOG_SET_FLUSH_LEVEL(Name, Level) RAD_LOG_CATEGORY(Name).SetFlushLevel(LogLevel::Level)
+
+#else
+
+#define RAD_LOG_CATEGORY(Name)
+#define RAD_LOG_CATEGORY_DECLARE(Name, OutputLevel)
+#define RAD_LOG_CATEGORY_DEFINE(Name)
+#define RAD_LOG(CategoryName, Level, Format, ...)
+#define RAD_SET_LOG_OUTPUT_LEVEL(Name, Level)
+#define RAD_SET_LOG_FLUSH_LEVEL(Name, Level)
+
+#endif
+
+RAD_END_NAMESPACE
+
+RAD_LOG_CATEGORY_DECLARE(Global, Verbose);
+#ifndef LogGlobal
+#define LogGlobal(Level, Format, ...) RAD_LOG(Global, Level, Format, ##__VA_ARGS__)
+#endif
