8d0a368830fc5adb9cbfb1efccab1fcb8e5c1b25
Jerry Ma <majunlichn@outlook.com>
2023-08-29 23:14:42 +0800

    install_vcpkg_packages
    Scripts to install required packages.

---
+++ install_vcpkg_packages.ps1
@@ -0,0 +1,48 @@
+Push-Location $PSScriptRoot
+Write-Host "Current working directory: $(Get-Location)"
+
+$vcpkgRoot = $Env:VCPKG_ROOT
+if ($null -eq $vcpkgRoot -or !(Test-Path $vcpkgRoot -PathType Container)) {
+    Write-Host "radcpp: You need to setup vcpkg and set `$Env:VCPKG_ROOT to its path."
+    Write-Host "radcpp: Please refer to: https://vcpkg.io/en/getting-started.html"
+    return
+}
+
+$vcpkgPackages = @(
+    "boost"
+    "gtest"
+    "opencl"
+)
+
+$vcpkgTriplet = $null
+if ($IsWindows) {
+    $vcpkgTriplet = "x64-windows"
+}
+
+$vcpkgExecutable = "vcpkg"
+if ($IsWindows) {
+    $vcpkgExecutable = "vcpkg.exe"
+}
+$vcpkgExecutable = Join-Path -Path $vcpkgRoot -ChildPath $vcpkgExecutable
+if (Test-Path -Path $vcpkgExecutable -PathType Leaf) {
+    Write-Host "Found vcpkg executable: $vcpkgExecutable"
+} else {
+    Write-Host "vcpkg executable does not exist!"
+    return
+}
+
+Push-Location $vcpkgRoot
+Write-Host "Current working directory: $(Get-Location)"
+Write-Host "Updating vcpkg:"
+Invoke-Expression "git pull"
+foreach ($package in $vcpkgPackages) {
+    Write-Host "Installing vcpkg package $package`:"
+    if ($null -ne $vcpkgTriplet) {
+        Invoke-Expression "& `"$vcpkgExecutable`" install $package`:$vcpkgTriplet"
+    } else {
+        Invoke-Expression "& `"$vcpkgExecutable`" install $package"
+    }
+}
+Pop-Location
+
+Pop-Location
