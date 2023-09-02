b8979c04b6ac0d22aebeb2103ea9f522a2729ecc
Jerry Ma <majunlichn@outlook.com>
2023-09-03 02:16:42 +0800

    libs/VulkanCore/VulkanCommon
    - vk_enum_string_helper.
    - error handling.

--- libs/VulkanCore/VulkanCommon.cpp
+++ libs/VulkanCore/VulkanCommon.cpp
@@ -1,3 +1,10 @@
 #define VOLK_IMPLEMENTATION
 #define VMA_IMPLEMENTATION
 #include "VulkanCommon.h"
+
+void LogVulkanError(VkResult result, const char* function, const char* file, uint32_t line)
+{
+    LogVulkan(Error, "%s failed with VkResult=%s(%d).",
+        function, string_VkResult(result), result, file, line);
+    throw VulkanError(result);
+}
--- libs/VulkanCore/VulkanCommon.h
+++ libs/VulkanCore/VulkanCommon.h
@@ -12,3 +12,23 @@
 #define VMA_STATIC_VULKAN_FUNCTIONS 0
 #define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
 #include "vk_mem_alloc.h"
+
+#include "helpers/vk_enum_string_helper.h"
+#include <exception>
+
+class VulkanError : public std::exception
+{
+public:
+    VulkanError(VkResult result) : m_result(result) {}
+    const char* what() const noexcept override { return string_VkResult(m_result); }
+private:
+    VkResult m_result;
+}; // class VulkanError
+
+RAD_LOG_CATEGORY_DECLARE(Vulkan, Verbose);
+#define LogVulkan(Level, Format, ...) RAD_LOG(Vulkan, Level, Format, ##__VA_ARGS__)
+
+// Check Vulkan return code and throw VulkanError if result < 0
+void LogVulkanError(VkResult result, const char* function, const char* file, uint32_t line);
+#define VK_CHECK(VulkanCall) \
+{ const VkResult r = VulkanCall; if (r < 0) { LogVulkanError(r, #VulkanCall, __FILE__, __LINE__); } }
