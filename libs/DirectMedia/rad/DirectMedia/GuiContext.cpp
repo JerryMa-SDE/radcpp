3baf2e57dfb99dffd6239264ab61ce0455805519
Jerry Ma <majunlichn@outlook.com>
2023-09-05 08:07:25 +0800

    libs/DirectMedia: GuiContext

---
+++ libs/DirectMedia/rad/DirectMedia/GuiContext.cpp
@@ -0,0 +1,56 @@
+#include "GuiContext.h"
+#include "rad/IO/Logging.h"
+
+namespace sdl
+{
+
+GuiContext::GuiContext(rad::Ref<Window> window, rad::Ref<Renderer> renderer) :
+    m_window(std::move(window)),
+    m_renderer(std::move(renderer))
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;   // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;    // Enable Gamepad Controls
+
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL2_InitForSDLRenderer(m_window->GetHandle(), m_renderer->GetHandle());
+    ImGui_ImplSDLRenderer2_Init(m_renderer->GetHandle());
+}
+
+GuiContext::~GuiContext()
+{
+    // Cleanup
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+}
+
+bool GuiContext::ProcessEvent(const SDL_Event& event)
+{
+    return ImGui_ImplSDL2_ProcessEvent(&event);
+}
+
+void GuiContext::StartFrame()
+{
+    // Start the Dear ImGui frame
+    ImGui_ImplSDLRenderer2_NewFrame();
+    ImGui_ImplSDL2_NewFrame();
+    ImGui::NewFrame();
+}
+
+void GuiContext::Render()
+{
+    ImGui::Render();
+}
+
+void GuiContext::RenderDrawData()
+{
+    ImGuiIO& io = ImGui::GetIO();
+    ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
+}
+
+} // namespace sdl
---
+++ libs/DirectMedia/rad/DirectMedia/GuiContext.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "rad/Core/Global.h"
+#include "Window.h"
+#include "Renderer.h"
+
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_sdl2.h"
+#include "imgui/backends/imgui_impl_sdlrenderer2.h"
+
+namespace sdl
+{
+
+class GuiContext : public rad::RefCounted<GuiContext>
+{
+public:
+    GuiContext(rad::Ref<Window> window, rad::Ref<Renderer> renderer);
+    ~GuiContext();
+
+    ImGuiIO& GetIO() { return ImGui::GetIO(); }
+
+    ImFontAtlas* GetFonts() { return ImGui::GetIO().Fonts; }
+
+    bool ProcessEvent(const SDL_Event& event);
+    void StartFrame();
+    void Render();
+    void RenderDrawData();
+
+private:
+    rad::Ref<Window> m_window;
+    rad::Ref<Renderer> m_renderer;
+
+}; // class GuiContext
+
+} // namespace sdl
