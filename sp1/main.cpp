6f2976118b1a17c4a78b70629b3ea3366854c656
Jerry Ma <majunlichn@outlook.com>
2023-10-18 16:30:19 +0800

    rad: support tclap
    https://tclap.sourceforge.net/manual.html

--- sp1/main.cpp
+++ sp1/main.cpp
@@ -1,7 +1,18 @@
 #include "gtest/gtest.h"
+#include "tclap/CmdLine.h"
 
 int main(int argc, char** argv)
 {
+    try
+    {
+        TCLAP::CmdLine cmdLine("sp1 command line controls", ' ');
+        cmdLine.parse(argc, argv);
+    }
+    catch (TCLAP::ArgException& e)
+    {
+        fprintf(stderr, "TCLAP::ArgException: %s\n", e.error().c_str());
+    }
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
