72dddf904c73ee4b95dd937c54bd3a425c59eeac
Jerry Ma <majunlichn@outlook.com>
2023-11-02 00:42:41 +0800

    3rdparty: get prebuilt libraries

---
+++ 3rdparty/get_prebuilt_libraries.ps1
@@ -0,0 +1,29 @@
+Push-Location $PSScriptRoot
+Write-Host "Current working directory: $(Get-Location)"
+
+$repos = @(
+    [PSCustomObject]@{
+        Name = "Compressonator"
+        URL = "https://github.com/JerryMa-SDE/Compressonator.git"
+    }
+)
+
+foreach ($repo in $repos)
+{
+    if (-not (Test-Path -Path $repo.Name -PathType Container))
+    {
+        Write-Host "$($repo.Name) doesn't exist, cloning from $($repo.URL) ..."
+        Invoke-Expression "git clone $($repo.URL) $($repo.Name)"
+    }
+    
+    if (Test-Path -Path $repo.Name -PathType Container)
+    {
+        Write-Host "$($repo.Name) exists, updating ..."
+        Push-Location $repo.Name
+        Invoke-Expression "git fetch"
+        Invoke-Expression "git pull"
+        Pop-Location
+    }
+}
+
+Pop-Location
