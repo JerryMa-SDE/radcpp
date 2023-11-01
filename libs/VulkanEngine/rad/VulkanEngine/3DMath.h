eafc458e0675383d1a7ef08beb53e74b5e8ba6df
Jerry Ma <majunlichn@outlook.com>
2023-11-01 22:14:18 +0800

    libs/VulkanEngine/3DMath
    GetMaxMipLevel

---
+++ libs/VulkanEngine/rad/VulkanEngine/3DMath.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "rad/Core/Math.h"
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
+
+uint32_t GetMaxMipLevel(uint32_t width, uint32_t height);
+uint32_t GetMaxMipLevel(uint32_t width, uint32_t height, uint32_t depth);
