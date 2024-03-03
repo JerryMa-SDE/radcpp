69679025d3f3ac32f8203f14dff07968152c616b
Jerry Ma <majunlichn@outlook.com>
2024-03-03 19:00:33 +0800

    rad/Core/Span

--- rad/Core/Global.h
+++ rad/Core/Global.h
@@ -3,6 +3,17 @@
 #include <cstddef>
 #include <cstdint>
 
+#if defined(__GNUC__) && defined(__GNUC_MINOR__) && defined(__GNUC_PATCHLEVEL__)
+#define RAD_GNUC_PREREQ(major, minor, patch) \
+            ((__GNUC__ << 20) + (__GNUC_MINOR__ << 10) + __GNUC_PATCHLEVEL__ >= \
+                ((major) << 20) + ((minor) << 10) + (patch))
+#elif defined(__GNUC__) && defined(__GNUC_MINOR__)
+#define RAD_GNUC_PREREQ(major, minor, patch) \
+            ((__GNUC__ << 20) + (__GNUC_MINOR__ << 10) >= ((major) << 20) + ((minor) << 10))
+#else
+#define RAD_GNUC_PREREQ(major, minor, patch) 0
+#endif
+
 namespace rad
 {
 
