57ebea170d1f194bbdb03ed143837284d57f3425
Jerry Ma <majunlichn@outlook.com>
2024-03-27 17:41:25 +0800

    rad/Core/Global: predef and attributes

--- rad/Core/Global.h
+++ rad/Core/Global.h
@@ -2,6 +2,8 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <intrin.h>
+#include "boost/predef.h"
 
 #if defined(__GNUC__) && defined(__GNUC_MINOR__) && defined(__GNUC_PATCHLEVEL__)
 #define RAD_GNUC_PREREQ(major, minor, patch) \
@@ -13,6 +15,26 @@
 #else
 #define RAD_GNUC_PREREQ(major, minor, patch) 0
 #endif
+
+#if BOOST_COMP_MSVC_EMULATED
+#define RAD_DEBUG_BREAK() [[unlikely]] __debugbreak()
+#define RAD_ASSUME(expr) __assume(expr)
+#else
+#include <signal.h>
+#define RAD_DEBUG_BREAK() [[unlikely]] raise(SIGTRAP)
+#define RAD_ASSUME(expr) __attribute__((assume(expr)))
+#endif
+
+// C++ Attributes: https://en.cppreference.com/w/cpp/language/attributes
+// [[noreturn]]
+// [[carries_dependency]]
+// [[deprecated]]/[[deprecated("reason")]]
+// [[fallthrough]]
+// [[nodiscard]]/[[nodiscard("reason")]]
+// [[maybe_unused]]
+// [[likely]]/[[unlikely]]
+// [[no_unique_address]]
+// [[assume(expression)]] // C++23
 
 namespace rad
 {
